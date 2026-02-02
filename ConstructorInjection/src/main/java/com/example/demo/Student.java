d920ca726f9a59a1618dd35a618f84bcb1404cc7
sksha <sksha@host.docker.internal>
2026-02-02 13:13:56 +0530

    Initial commit.

---
+++ ConstructorInjection/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	Laptop ref;
+	void study() {
+		System.out.println("Student is studying");
+	}
+	void useLaptop() {
+		ref.work();
+	}
+	@Autowired
+	public Student(Laptop ref) {
+		this.ref = ref;
+	}
+	
+
+}
